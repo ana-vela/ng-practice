08f093f84bf486fb287f0d90a23ca1af07d2d46f
Ana Vela <avela7@cnm.edu>
2016-11-17 10:17:55 -0700

    added public_html and templates

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome Ng Practice!</h1>
+			<p class="lead">This is a bare-bones Angular 2 Front End :D</p>
+		</div>
+	</div>
+</section>
